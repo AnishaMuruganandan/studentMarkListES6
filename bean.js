38e5b134a9955759c5cbdedae89f3e881541cb44
AnishaMuruganandan <anisha95.m@gmail.com>
2018-05-30 23:14:51 +0530

    es6

---
+++ bean.js
@@ -0,0 +1,34 @@
+class Bean {
+constructor(studentName, studentRollNo, studentStd, studentSchoolName) {
+      this.studentName = studentName;
+      this.studentRollNo = studentRollNo;
+      this.studentStd = studentStd;
+      this.studentSchoolName = studentSchoolName;
+    }
+    get Nameandmarks() {
+      return this.studentName;
+    }
+    set Nameandmarks(nameandmarks) {
+      this.studentName = studentName;
+    }
+    get Nameandmarks() {
+      return this.studentRollNo;
+    }
+    set Nameandmarks(nameandmarks) {
+      this.studentRollNo = studentRollNo;
+    }
+    get Nameandmarks() {
+      return this.studentStd;
+    }
+    set Nameandmarks(nameandmarks) {
+      this.studentStd = studentStd;
+    }
+    get Nameandmarks() {
+      return this.studentSchoolName;
+    }
+    set Nameandmarks(nameandmarks) {
+      this.studentSchoolName = studentSchoolName;
+    }
+
+
+}
